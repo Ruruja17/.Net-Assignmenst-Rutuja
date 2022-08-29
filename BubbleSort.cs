ca4bbfe78846b18234cb727c2847fa4c1134b2ca
Ruruja17 <111499781+Ruruja17@users.noreply.github.com>
2022-08-29 10:09:16 +0530

    Add files via upload

---
+++ BubbleSort.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace demoproject
+{
+    internal class BubbleSort
+    {
+       
+           static void Main(string[] args)
+        {
+            Console.WriteLine(" Bubble Sort ");
+
+            int[] Num = { 54, 45, 90, 22, 17, 71,8,12 };
+            int temp;
+
+            for (int i = 0; i < Num.Length; i++)
+            
+            {
+                for (int j = 0; j < Num.Length - 1 - i; j++)
+                { 
+                    if (Num[j] > Num[j + 1]) 
+                    { 
+                        temp = Num[j]; Num[j] = Num[j + 1];
+                        Num[j + 1] = temp; 
+                    }
+                } }
+
+            foreach (int a in Num) { Console.Write(a + " "); }
+
+            Console.ReadLine();
+
+        }
+
+
+        
+    }
+}
