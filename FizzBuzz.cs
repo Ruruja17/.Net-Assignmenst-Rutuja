747c8deb269187891e61276290d06c4e51f2d7ad
Ruruja17 <111499781+Ruruja17@users.noreply.github.com>
2022-08-29 10:12:55 +0530

    Add files via upload

---
+++ FizzBuzz.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace demoproject
+{
+    internal class FizzBuzz
+    {
+        //static void Main(string[] args)
+        //{
+        //    int Num;
+            
+        //    for(Num = 1; Num <=100; Num++)
+        //    {
+        //        if (Num%3 == 0 && Num%5 ==0)
+        //        {
+        //            Console.WriteLine("FizzBuzz : " +Num);
+        //        }
+        //        else if  (Num % 3 == 0)
+        //        {
+
+        //            Console.WriteLine("Fizz : " + Num);
+        //        }
+        //        else if(Num%5== 0)
+        //        {
+        //            Console.WriteLine("Buzz : " +Num);
+        //        } 
+        //        else
+        //        {
+        //            Console.WriteLine(Num);
+        //        }
+                
+
+        //    }
+        //    Console.ReadLine();
+        //}
+    }
+}
