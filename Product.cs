eaa75a35d4b0d265df657ef35f8eecacf4fef3aa
Ruruja17 <111499781+Ruruja17@users.noreply.github.com>
2022-08-29 10:27:44 +0530

    Add files via upload

---
+++ Product.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShoppingLib
+{
+    public class Product
+    {
+        private string productName;
+
+        public string ProductName
+        {
+            get { return productName; }
+            set { productName = value; }
+        }
+
+        private double price;
+
+        public double Price
+        {
+            get { return price; }
+            set { price = value; }
+
+        }
+
+        private int quantity;
+
+        public int Quantity
+        {
+            get { return quantity; }
+            set { quantity = value; }
+        }
+
+        public double GetProductcCost()
+        {
+            return Quantity * Price;
+        }
+
+        public override string ToString()
+        {
+            return String.Format($"ProductName : {ProductName} Quantity : {Quantity} Price: {price} Cost: {GetProductcCost()} ");
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -3,31 +3,43 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using PersonalInfoLib;
+using ShoppingLib;
 
-namespace PersonDataTestApp
+namespace TestShopping
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Person person = new Person();
-            person.Fname = "Rutuja";
-            person.Lname = "Jadhav";
-            person.Email = "rutujajadhav175200@gmail.com";
-            person.DateOfBirth = Convert.ToDateTime("05/17/2000");
+            Product product = new Product();
+           
+            
+            product.ProductName = "JOHNSON";
+            product.Price = 200;
+            product.Quantity = 23;
 
-            Person person1 = new Person();
-            person1.StartDate = Convert.ToDateTime("12/22");
-            person1.EndDate = Convert.ToDateTime("01/19");
+            Console.WriteLine(product);
+
+            Product product1 = new Product();
 
 
-            Console.WriteLine(person);
-            Console.WriteLine("Is person is Adult ? = " + person.IsAdult());
-            Console.WriteLine("Is persons birthday ? = " + person.IsBirthdayToday());
-            Console.WriteLine("Sunsign of Person  ? = " + person.SunSign());
+            product1.ProductName = "Noodles";
+            product1.Price = 12;
+            product1.Quantity = 50;
+
+            Console.WriteLine(product1);
+
+            product1.ProductName = "Nivya";
+            product1.Price = 123;
+            product1.Quantity = 15;
+
+            Console.WriteLine(product1);
+
+
+
+
             Console.ReadLine();
+
         }
-      
     }
 }
