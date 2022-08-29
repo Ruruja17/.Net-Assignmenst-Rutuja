401e696982ac4d49520849697ee452265e6cd242
Ruruja17 <111499781+Ruruja17@users.noreply.github.com>
2022-08-29 10:06:38 +0530

    Add files via upload

---
+++ Rectangle.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoLib
+{
+    public class Rectangle
+    {
+        private int length;
+
+        public int Length
+        {
+            get { return length; }
+            set { length = value; }
+
+        }
+
+        private int width;
+
+        public int Width
+        {
+            get { return width; }
+            set { width = value; }
+        }
+
+        public int GetArea()
+        {
+            return Length * Width;
+        }
+
+        public override string ToString()
+        {
+            return String.Format($"Length : {Length}  Width :{Width}");
+        }
+    }
+}
