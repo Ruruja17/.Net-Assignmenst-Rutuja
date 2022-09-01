921d1cb12c75a9641eeb9767f1bdead22da82174
Ruruja17 <111499781+Ruruja17@users.noreply.github.com>
2022-09-01 10:08:12 +0530

    Add files via upload

---
+++ Swap.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace demoproject
+{
+    public class Swap
+    {
+        private int num1 = 10;
+
+        public int Display()
+        {
+            return num1;
+        }
+        
+    }
+
+    public class Swap2
+    {
+
+        private int num3 = 20;
+
+        public int Display1()
+        {
+            return num3;
+        }
+        static void Main(string[] args)
+        {
+            Swap s = new Swap();
+            int num2 = s.Display();
+            Swap2 s1 = new Swap2();
+            int num4 = s1.Display1();
+            
+            
+
+            Console.WriteLine("before swapping:"  +num2);
+            Console.WriteLine("before swapping:"  + num4);
+            
+            SwapNum( num2, num4);
+
+            Console.WriteLine("After Swaping:"+ num2);
+            Console.WriteLine("After Swaping:" + num4);
+
+
+
+            Console.ReadLine();
+        }
+
+        static void SwapNum( int num2,  int num4)
+        {
+            int temp;
+            temp = num2;
+            num2 = num4;
+            num4 = temp;
+
+            
+
+        }
+
+
+       
+    }
+    
+    
+}
